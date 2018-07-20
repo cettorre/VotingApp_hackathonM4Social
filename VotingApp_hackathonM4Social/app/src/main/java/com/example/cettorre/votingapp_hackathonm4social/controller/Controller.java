c3a14e8ffcc3cb0d480d5b5bdd26364a83b4a620
Unknown <c.ettorre@gmail.com>
2018-07-20 12:31:18 +0200

    added classes for network communication with the server
    added HttpHelper NetworkHelper RequestPackege MyService

--- VotingApp_hackathonM4Social/app/src/main/java/com/example/cettorre/votingapp_hackathonm4social/controller/Controller.java
+++ VotingApp_hackathonM4Social/app/src/main/java/com/example/cettorre/votingapp_hackathonm4social/controller/Controller.java
@@ -1,4 +1,8 @@
 package com.example.cettorre.votingapp_hackathonm4social.controller;
 
 public class Controller {
+    //https://cettorre.github.io/serverresponse.json
+    //http://560057.youcanlearnit.net/services/json/itemsfeed.php
+
+
 }
--- VotingApp_hackathonM4Social/app/src/main/java/com/example/cettorre/votingapp_hackathonm4social/view/user/UserHomeActivity.java
+++ VotingApp_hackathonM4Social/app/src/main/java/com/example/cettorre/votingapp_hackathonm4social/view/user/UserHomeActivity.java
@@ -1,14 +1,41 @@
 package com.example.cettorre.votingapp_hackathonm4social.view.user;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.cettorre.votingapp_hackathonm4social.R;
+import com.example.cettorre.votingapp_hackathonm4social.model.DataItem;
+import com.example.cettorre.votingapp_hackathonm4social.services.MyService;
+import com.example.cettorre.votingapp_hackathonm4social.utils.NetworkHelper;
+import com.example.cettorre.votingapp_hackathonm4social.utils.RequestPackage;
 
 public class UserHomeActivity extends AppCompatActivity {
+    private static final String JSON_URL =
+            "http://560057.youcanlearnit.net/services/json/itemsfeed.php";
+    private boolean networkOk;
+    TextView output;
+
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            DataItem[] dataItems = (DataItem[]) intent
+                    .getParcelableArrayExtra(MyService.MY_SERVICE_PAYLOAD);
+            for (DataItem item : dataItems) {
+                output.append(item.getItemName() + "\n");
+            }
+        }
+    };
+
+
 
 
     ImageButton imageButton;
@@ -26,5 +53,41 @@ public class UserHomeActivity extends AppCompatActivity {
         }
     });
 
+//---------------------------------------------------
+
+        output = (TextView) findViewById(R.id.output);
+
+        LocalBroadcastManager.getInstance(getApplicationContext())
+                .registerReceiver(mBroadcastReceiver,
+                        new IntentFilter(MyService.MY_SERVICE_MESSAGE));
+
+        networkOk = NetworkHelper.hasNetworkAccess(this);
+        output.append("Network ok: " + networkOk);
+
+        runClickHandler();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(getApplicationContext())
+                .unregisterReceiver(mBroadcastReceiver);
+    }
+
+    public void runClickHandler() {
+
+        if (networkOk) {
+            RequestPackage requestPackage = new RequestPackage();
+            requestPackage.setEndPoint(JSON_URL);
+            requestPackage.setParam("category", "Entrees");
+            requestPackage.setMethod("POST");
+
+            Intent intent = new Intent(this, MyService.class);
+            intent.putExtra(MyService.REQUEST_PACKAGE, requestPackage);
+            startService(intent);
+        } else {
+            Toast.makeText(this, "Network not available!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
